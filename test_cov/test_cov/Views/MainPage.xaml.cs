04243082db4e1691cb3f0964d04cc02f9b77fd4c
Alexandru Smolenschi <smolenschialexandru@gmail.com>
2020-03-26 12:14:56 +0100

    Добавьте файлы проекта.

---
+++ test_cov/test_cov/Views/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace test_cov
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage(new Views.covid()) 
+            {
+                 BarBackgroundColor = Color.FromHex("#000")
+            }; 
+           
+
+            IsPresented = false;
+        }
+
+        private void bnt_1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Views.covid())
+            {
+                BarBackgroundColor = Color.FromHex("#000")
+            };
+            IsPresented = false;
+        }
+
+        private void bnt_2(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Views.Simptome())
+            {
+                BarBackgroundColor = Color.FromHex("#000")
+            };
+            IsPresented = false;
+        }
+    }
+}
